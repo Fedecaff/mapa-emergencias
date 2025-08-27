ad5c6c7bbbf4f765dd2bb90e662cb39ac73552c0
Federico Caffettaro <federico.gomez.sc@gmail.com>
2025-08-27 13:52:11 -0300

    Corregir error de sintaxis en nodemailer - createTransporter -> createTransport

--- src/servicios/emailService.js
+++ src/servicios/emailService.js
@@ -17,7 +17,7 @@ class EmailService {
             }
 
             // Configurar transporter para Gmail
-            this.transporter = nodemailer.createTransporter({
+            this.transporter = nodemailer.createTransport({
                 service: 'gmail',
                 auth: {
                     user: process.env.EMAIL_USER,
